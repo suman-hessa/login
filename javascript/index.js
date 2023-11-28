181d47041ac70abe1d4417227542a9946dfd23a0
suman-hessa <sumanhessa001@gmail.com>
2023-11-28 23:09:22 +0530

    28 Nov 2023

--- javascript/index.js
+++ javascript/index.js
@@ -2,7 +2,7 @@
 
 making a faulty calculator : 
 
-*/
+
 
     let a = prompt("Enter first number");
     let op = prompt("operation");
@@ -44,5 +44,56 @@ making a faulty calculator :
             console.log(a/b);
             break;
     }
+       }
+
+       */
+
+       /*  Create a business name generator by combining list of adjectives and shop name and another word  */
+       
+       function generateBusinessName(){
+        //Adjective :
+        
+           let first = '';
+        let Random1 = Math.random();
+        if(Random1>=0.0 && Random1<=0.33){
+                first = "Crazy";
+        }
+        else if(Random1>0.33 && Random1<=0.66){
+                 first = "Amazing";
+        }
+        else{
+               first = "Fire";
+        }
+
+        // Shop name : 
+                let second = '';
+        let Random2 = Math.random();
+  
+        if(Random2>=0.0 && Random2<=0.33){
+                second = "Engine";
+        }
+        else if(Random2>0.33 && Random2<=0.66){
+                 second = "Food";
+        }
+        else{
+               second = "Garments";
+        }
+            
+        // Another word : 
+              let third = "";
+             
+        let Random3 = Math.random();
+    
+        if(Random3>=0.0 && Random3<=0.33){
+                third = "Bros";
+        }
+        else if(Random3>0.33 && Random3<=0.66){
+                 third = "Limited";
+        }
+        else{
+               third = "Hub";
+        }
+
+        console.log(`Your business name is ${first+second+third}`);
        }
 
